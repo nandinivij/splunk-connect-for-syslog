abae1a3f64ce5130cb2b1ff5301422161342b930
Nandini Vij <nvij@splunk.com>
2020-06-19 16:17:56 -0700

    [TEST] column not working

--- scratch.sh
+++ scratch.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
+      __commitlist="What's New "$'\n'
       declare -a ReleaseColumn=("CI" "DOC" "FIX" "FEATURE" "FILTERADD" "FILTERMOD" "REVERT" "TEST")
       for col in ${ReleaseColumn[@]};
       do
         __columnNotes=()
         while IFS= read -r line; do
             __columnNotes+=( "$line" )
-        done < <( (git log --pretty=format:"%s" -i -E --grep="^(\[$col\])"$finalversion...... |cat) )
-        __commitlist="[$col]"$'\n'
+        done < <( (git log --pretty=format:"%s" -i -E --grep="^(\[$col\])"$finalversion... |cat) )
+        __commitlist+="[$col]"$'\n'
         for i in "${__columnNotes[@]}"
         do
           IFS=' '
           read -ra my_array2 <<< $i
           __commitlist+="${my_array2[1]}"$'\n'
         done
-        echo $__commitlist
       done
+      echo $__commitlist
 # Declare an array of string with type
 #declare -a ReleaseColumn=("CI" "DOC" "FIX" "FEATURE" "FILTERADD" "FILTERMOD" "REVERT" "TEST")
 #for col in ${ReleaseColumn[@]}; do
