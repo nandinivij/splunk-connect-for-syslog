9869dc7022c03b37bff1215b0437dfb5c580522a
Nandini Vij <nvij@splunk.com>
2020-08-17 13:44:40 -0700

    trying to start publish

--- tests/test_juniper_junos_rfc3164.py
+++ tests/test_juniper_junos_rfc3164.py
@@ -1,8 +1,4 @@
 # Copyright 2019 Splunk, Inc.
-#
-# Use of this source code is governed by a BSD-2-clause-style
-# license that can be found in the LICENSE-BSD2 file or at
-# https://opensource.org/licenses/BSD-2-Clause
 
 from jinja2 import Environment
 
